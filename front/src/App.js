0e8f81ccf8d0c6df015da5c00c6f19838f7e12fe
frux <115709849+NATHANxSCARED@users.noreply.github.com>
2025-08-23 18:08:13 +0000

    init portfolio frontend

---
+++ front/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Head'>
+        <h1>Nathan Vittoni</h1>
+        <ul>
+          <li><a href='#home'>Home</a></li>
+          <li><a href='#Projects'>Projects</a></li>
+          <li><a href='#ContactMe'>Contact me</a></li>
+
+        </ul>
+      </div>
+
+      <div className='Main'>
+        <div className='card'>
+          <p>lorem ipsum vitatew riowejriowjro weriojwe oriowejriowe iorwej oirjwe rjweiorj iowe  jriowe jrweiojrweio weior weiojrwe iorwejrow eorjweoir jowejrwe orjweioweio jrweioriowe jriowejr owejr </p>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
